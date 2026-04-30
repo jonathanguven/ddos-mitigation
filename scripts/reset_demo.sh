e5f972bfe5d279b4edc79cdc814b97e68e394605
Jonathan Nguyen <jonathanguven@gmail.com>
2026-04-30 16:40:49 -0700

    fix reset button

--- scripts/reset_demo.sh
+++ scripts/reset_demo.sh
@@ -10,6 +10,6 @@ if command -v ovs-ofctl >/dev/null 2>&1; then
   sudo -n ovs-ofctl -O OpenFlow13 del-meters s1 2>/dev/null || true
 fi
 
-rm -f /tmp/h*_normal.log /tmp/h*_single_source_flood.log /tmp/h*_multi_source_flood.log
+sudo rm -f /tmp/h*_normal.log /tmp/h*_single_source_flood.log /tmp/h*_multi_source_flood.log
 
 echo "Demo reset requested."
